471bf186b2df6a57e16b3d47191196840cc52df8
James Taylor <James.steve.taylor@gmail.com>
2023-11-26 13:03:56 +0000

    Week 6 labs complete

--- labWork/weekSixLab.sql
+++ labWork/weekSixLab.sql
@@ -50,5 +50,26 @@ ALTER TABLE cd
     ADD COLUMN rating ENUM ('favourite', 'excel-lent', 'good', 'so-so', 'poor', 'rubbish');
 
 -- Part 3:
+set foreign_key_checks=0;
 ALTER TABLE artist
-    MODIFY id_number INT UNSIGNED PRIMARY KEY AUTO_INCREMENT;
+    MODIFY id_number INT UNSIGNED AUTO_INCREMENT ;
+set foreign_key_checks=1;
+
+-- Part 4:
+INSERT INTO artist (artist_name, webpage, date_of_birth)
+VALUES ('Bruce Springsteen','www.brucespringsteen.net', '1949-09-23');
+
+-- Part 5:
+SELECT id_number, bio, email 
+FROM artist;
+
+-- Part 6:
+INSERT INTO record_label (label_name)
+VALUES ('Columbia');
+
+INSERT INTO cd (catalog_no, title, publication_date, released_by, is_fronted_by, rating)
+VALUES ('42', 'Magic', '2010-11-02', 'Columbia', '1', 'excel-lent');
+
+UPDATE cd 
+SET publication_date = '2008-10-03'
+WHERE catalog_no = 42;
